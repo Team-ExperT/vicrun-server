dd0ddcd7cb7d6a35d50f29bf570619d7463002aa
spondbob <spondbob@eamca.com>
2016-04-03 16:06:11 +1000

    use substances of ni,ox,ph,ts

--- db/migrate/20160401010930_create_water_qualities.rb
+++ db/migrate/20160401010930_create_water_qualities.rb
@@ -3,18 +3,10 @@ class CreateWaterQualities < ActiveRecord::Migration
     create_table :water_qualities do |t|
       t.string  :area
       t.string  :region
-      t.integer :am_n
-      t.float   :am_min
-      t.float   :am_p25
-      t.float   :am_p50
-      t.float   :am_p75
-      t.float   :am_max
-      t.integer :ts_n
-      t.float   :ts_min
-      t.float   :ts_p25
+      t.float   :ni_p50
+      t.float   :ox_p50
+      t.float   :ph_p50
       t.float   :ts_p50
-      t.float   :ts_p75
-      t.float   :ts_max
       t.float   :latitude
       t.float   :longitude
       t.string  :area_id
