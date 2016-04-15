8e75f5e951cdfc84003c82bb621fdc50a3d8d0aa
spondbob <spondbob@eamca.com>
2016-04-15 20:17:19 +1000

    use redis caching

--- app/models/water_quality.rb
+++ app/models/water_quality.rb
@@ -1,2 +1,7 @@
 class WaterQuality < ActiveRecord::Base
+  def self.daily_picks
+    Rails.cache.fetch('daily_picks', expires_in: 1.day) do
+      self.limit(3).order("RANDOM()")
+    end
+  end
 end
