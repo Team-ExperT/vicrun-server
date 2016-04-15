f5525a2c8948885544ef858845fcfa2f7d8ab4ca
spondbob <spondbob@eamca.com>
2016-04-15 23:20:01 +1000

    use keys for randomize levels

--- app/models/water_quality.rb
+++ app/models/water_quality.rb
@@ -1,7 +1,33 @@
 class WaterQuality < ActiveRecord::Base
   def self.daily_picks
-    Rails.cache.fetch('daily_picks', expires_in: 1.day) do
-      self.limit(3).order("RANDOM()")
+    ids = []
+    keys = [57, 12, 35]
+    data = []
+
+    keys.each do |key|
+      rand_id = self.get_rand_id(key)
+      while true
+        if ids.include?(rand_id)
+          time = Time.new
+          rand_id = self.get_rand_id(key*time.sec)
+        else
+          ids << rand_id
+          data << self.find(rand_id)
+          break
+        end
+      end
     end
+
+    data
+    # Rails.cache.fetch('daily_picks', expires_in: 1.day) do
+    #   self.limit(3).order("RANDOM()")
+    # end
+  end
+
+  private
+
+  def self.get_rand_id(key)
+    time = Time.new
+    rand_id = ((time.month*key) + (time.year*key) + (time.day)) % 129
   end
 end
