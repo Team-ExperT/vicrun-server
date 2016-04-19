761d26eb57b16e0f542137ece6fdc461d9540a12
spondbob <spondbob@eamca.com>
2016-04-19 15:31:58 +1000

    refactor code

--- app/models/water_quality.rb
+++ app/models/water_quality.rb
@@ -7,51 +7,27 @@ class WaterQuality < ActiveRecord::Base
     ids = []
     keys = [57, 12, 35]
     data = []
+    result = []
 
-    garbage_list = self.where(garbage_active: 1, water_active: 0)
-    water_list = self.where(garbage_active: 0, water_active: 1)
-    hybrid_list = self.where("(garbage_active = ? AND water_active = ?) OR (garbage_active = ? AND water_active = ?)", 0,0,1,1)
+    data << self.where(garbage_active: 1, water_active: 0)
+    data << self.where(garbage_active: 0, water_active: 1)
+    data << self.where("(garbage_active = ? AND water_active = ?) OR (garbage_active = ? AND water_active = ?)", 0,0,1,1)
 
-    garbage = garbage_list[self.get_rand_id(1, 57, garbage_list.count)]
-    water = water_list[self.get_rand_id(1, 12, water_list.count)]
-    hybrid = hybrid_list[self.get_rand_id(1, 35, hybrid_list.count)]
-
-    [garbage, water, hybrid]
-
-    # keys.each do |key|
-    #   n = 1
-    #   while true
-    #     rand_id = self.get_rand_id(n, key, 129)
-    #     if ids.include?(rand_id)
-    #       time = Time.new
-    #       n = n + 1
-    #     else
-    #       ids << rand_id
-    #       data << self.find(rand_id)
-    #       break
-    #     end
-    #   end
-    # end
-
-    # data
-    # Rails.cache.fetch('daily_picks', expires_in: 1.day) do
-    #   self.limit(3).order("RANDOM()")
-    # end
+    data.each_with_index do |d, i|
+      total = d.count
+      id = self.get_rand_id(keys[i], total)
+      result << d[id]
+    end
+    
+    result
   end
 
   private
 
-  def self.get_rand_id(n, key, total)
+  def self.get_rand_id(key, total)
     time = Time.new
-    rand_id = (calc_d(n, time.month, key) + 
-      calc_d(n, time.year, key) +
-      calc_d(n, time.day, key)) % total
-  end
-
-  def self.calc_d(n, d, key) 
-    n.times do
-      d = d * key
-    end
-    d
+    rand_id = ((time.month * key) + 
+      (time.year * key) +
+      (time.day * key)) % total
   end
 end
