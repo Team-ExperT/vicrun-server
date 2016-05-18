c2eb2108e92d03e1d546aadccf4fd3ab51be347e
spondbob <spondbob@eamca.com>
2016-05-18 22:24:31 +1000

    add monash_stage to daily_levels

--- app/controllers/api/water_qualities_controller.rb
+++ app/controllers/api/water_qualities_controller.rb
@@ -38,6 +38,14 @@ class Api::WaterQualitiesController < ApplicationController
       end
     end
 
+    option = Option.find_by(:key => 'monash_expo_mode')
+    monash_expo_mode = option.value
+
+    if monash_expo_mode.eql? "true"
+      monash_stage = {"garbage_active":1,"water_active":1,"id":999,"area":"Monash University","region":"Monash Caulfield","x":315,"y":319}
+      levels.push(monash_stage)
+    end
+
     respond_with(levels)
   end
 
